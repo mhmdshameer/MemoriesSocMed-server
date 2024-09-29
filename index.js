08c1e4892cae0b9be9a244b2da04da64b2a0a03a
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-09-29 12:11:54 +0530

    Another commit

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js"; 
+import dotenv from "dotenv"
 
 const app = express();
 const port = process.env.PORT || 5000;
+dotenv.config();
+
 app.use(bodyParser.json({limit:"30mb", extended:"true"}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:"true"}));
 app.use(cors())
-const url = 'mongodb://localhost:27017/memoriesMern'; 
+app.use("/posts",postRoutes);    
+const url = process.env.CONNECTING_URL ; 
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(port,()=> console.log(`App is running on port ${port}`)))
@@ -17,7 +21,6 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
         console.error("Connection error", err);
     });
 
-app.use("/posts",postRoutes);    
 
 
 
