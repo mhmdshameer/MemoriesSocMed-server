36efe35ca3d367abf911a76ad6e1038b04f92909
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-10 13:38:09 +0530

    feat(server): add pagination support for fetching posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,9 +5,15 @@ import PostMessage from "../models/postMessage.js";
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
+   const {page} = req.query;
   try {
-    const postMessages = await PostMessage.find();
-    res.status(200).json(postMessages);
+    const LIMIT = 8 ;
+    const startIndex =(Number(page)-1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
+
+    const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
+
+    res.status(200).json({data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total/LIMIT)});
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -20,8 +26,6 @@ export const getSearchPosts = async (req,res) =>{
 
     try {
       const title = new RegExp(searchQuery, 'i');
-      console.log("title:",title)
-      console.log("tags:",tags)
       const posts = await PostMessage.find({ $or: [{title}, {tags: { $in: tags.split(',')}}]})
       res.json({data: posts})
     } catch (error) {
@@ -38,7 +42,6 @@ export const createPost = async (req, res) => {
     createdAt: new Date().toISOString(),
   });
   try {
-    console.log(newPost);
     await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
