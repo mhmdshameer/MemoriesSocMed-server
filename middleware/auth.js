7b0a49001f23e2c802e3fab3aedfc31bfbcd852f
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-12 06:30:03 +0530

    feat(server): implement backend logic to retrieve full post details

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,8 @@
 import jwt, { decode } from "jsonwebtoken"
 
 const auth = async (req, res, next) => {
-  console.log("reached auth middleware")
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token)
     const isCustomsAuth = token.length < 500;
 
     let decodeData;
